238b824eb2ceae0eeb7f853de578bfef424ca09a
Marisa Kirisame <soymilk@airmail.cc>
2018-05-16 19:21:09 -0400

    MetalInfos are global

--- corrosion.py
+++ corrosion.py
@@ -23,17 +23,20 @@ class Leaf(Node):
 
 class MetalInfo:
 
-    # argument is tuples of key:value pairs for dictionary
     def __init__(self, starting_dict):
-        #self.picture = pairs.get("picture")
-        #self.price = pairs.get("price")
-        #self.melting_pt = pairs.get("melting_pt")
-        #self.tensile_str = pairs.get("tensile_str")
-        #self.density = pairs.get("density")
-        #self.other_facts = pairs.get("other_facts") # list of other facts
-
         self.info = starting_dict
-        
+
+ss_304 = MetalInfo({
+        "name":"SS 304",
+        "picture":"",
+        "Price":"4.69",
+        "Density":"490 - 500",
+        "Tensile strength":"74 - 90",
+        "Thermal conductivity":"2.62 - 3.18",
+        "Melting point":"2552 - 2642",
+        "Specific Heat":"0.37 - 0.41"
+    })
+
 def info_into_frame(frame, metal_info):
     return None
         
@@ -47,17 +50,6 @@ def main():
     frame.grid(column=0, row=0, sticky=(N, S, E, W))
     frame.columnconfigure(0, weight=1)
     frame.rowconfigure(0, weight=1)
-
-    ss_304 = MetalInfo({
-        "name":"SS 304",
-        "picture":"",
-        "Price":"4.69",
-        "Density":"490 - 500",
-        "Tensile strength":"74 - 90",
-        "Thermal conductivity":"2.62 - 3.18",
-        "Melting point":"2552 - 2642",
-        "Specific Heat":"0.37 - 0.41"
-    })
 
     root.mainloop()
 
