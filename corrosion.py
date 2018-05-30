68d9e51d60c8f8b03bf9424851ad960049446768
Marisa Kirisame <soymilk@airmail.cc>
2018-05-30 18:54:13 -0400

    Trimmed leaf frame filling funcs

--- corrosion.py
+++ corrosion.py
@@ -25,37 +25,23 @@ def read_metal_info(filename):
     return metal_infos
 
 def info_into_frame(frame, metal_info):
-    name = ttk.Label(frame, text=metal_info.info["name"])
-    name.grid(column=1, row=1)
-    price = ttk.Label(frame, text=metal_info.info["price"])
-    price.grid(column=1, row=2)
-    density = ttk.Label(frame, text=metal_info.info["density"])
-    density.grid(column=1, row=3)
-    tensile = ttk.Label(frame, text=metal_info.info["tensile_strength"])
-    tensile.grid(column=1, row=4)
-    thermal = ttk.Label(frame, text=metal_info.info["thermal_conductivity"])
-    thermal.grid(column=1, row=5)
-    melting = ttk.Label(frame, text=metal_info.info["melting_point"])
-    melting.grid(column=1, row=6)
-    specific = ttk.Label(frame, text=metal_info.info["specific_heat"])
-    specific.grid(column=1, row=7)
+    Label(frame, text=metal_info.info["name"]).grid(column=1, row=1)
+    Label(frame, text=metal_info.info["price"]).grid(column=1, row=2)
+    Label(frame, text=metal_info.info["density"]).grid(column=1, row=3)
+    Label(frame, text=metal_info.info["tensile_strength"]).grid(column=1, row=4)
+    Label(frame, text=metal_info.info["thermal_conductivity"]).grid(column=1, row=5)
+    Label(frame, text=metal_info.info["melting_point"]).grid(column=1, row=6)
+    Label(frame, text=metal_info.info["specific_heat"]).grid(column=1, row=7)
 
-# these two could be genericized together but it isn't worth the effort
+## these two could be genericized together but it isn't worth the effort
 def label_into_frame(frame):
-    name = ttk.Label(frame, text="")
-    name.grid(column=1, row=1)
-    price = ttk.Label(frame, text="Price")
-    price.grid(column=1, row=2)
-    density = ttk.Label(frame, text="Density")
-    density.grid(column=1, row=3)
-    tensile = ttk.Label(frame, text="Tensile strength")
-    tensile.grid(column=1, row=4)
-    thermal = ttk.Label(frame, text=metal_info.info["Thermal conductivity"])
-    thermal.grid(column=1, row=5)
-    melting = ttk.Label(frame, text=metal_info.info["Melting point"])
-    melting.grid(column=1, row=6)
-    specific = ttk.Label(frame, text=metal_info.info["Specific heat"])
-    specific.grid(column=1, row=7)
+    Label(frame, anchor="w", text="").grid(column=1, row=1)
+    Label(frame, anchor="w", text="Price").grid(column=1, row=2)
+    Label(frame, anchor="w", text="Density").grid(column=1, row=3)
+    Label(frame, anchor="w", text="Tensile strength").grid(column=1, row=4)
+    Label(frame, anchor="w", text="Thermal conductivity").grid(column=1, row=5)
+    Label(frame, anchor="w", text="Melting point").grid(column=1, row=6)
+    Label(frame, anchor="w", text="Specific heat").grid(column=1, row=7)
 
 ## fills a frame with a grid of frames
 def fill_frame(mainframe, width, height):
@@ -125,23 +111,21 @@ class TreeStepper:
             current_button.grid(column=0, row=0)
             i += 1
 
-        # Debug
-        #self.output = Label(self.frames[i],
-        #                    text=self.current_tag[0].attrib["value"],
-        #                    wraplength=100)
-        #self.output.grid(column=0, row=0)
-
     def leaves_to_labels(self):
         i = 0
+        length = len(list(self.current_tag))
+        self.regrid(length + ceil(length / gui_columns)) # add one more frame to each row for labels
         for leaf in self.current_tag:
-            info_into_frame(self.frames[i], self.metal_infos[leaf.attrib["value"]])
+            if i % gui_columns == 0:
+                label_into_frame(self.frames[i])
+            else:
+                info_into_frame(self.frames[i], self.metal_infos[leaf.attrib["value"]])
             i += 1
 
     def evaluate_current(self):
         """ Act based on current tag """
         if self.current_tag.tag == "Option":
             if self.current_tag[0].tag == "Leaf":
-                self.regrid(len(list(self.current_tag)))
                 self.leaves_to_labels()
             else:
                 self.current_tag = self.current_tag[0] 
